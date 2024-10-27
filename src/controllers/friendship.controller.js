2c3c26abf31fad7d03a558a4336d50a74254518f
suraj946 <sgsuraj150@gmail.com>
2024-10-27 16:35:06 +0545

    create a search in friend controller and fixed some bugs with optimization

--- src/controllers/friendship.controller.js
+++ src/controllers/friendship.controller.js
@@ -20,29 +20,36 @@ export const createFriendRequest = asyncHandler(async (req, res, next) => {
     return next(new ApiError(BAD_REQUEST, "Please provide user id"));
   }
   if (receiverId.toString() === req.user._id?.toString()) {
-    return next(new ApiError(BAD_REQUEST, "Haha!! you can't send request to yourself"));
+    return next(
+      new ApiError(BAD_REQUEST, "Haha!! you can't send request to yourself")
+    );
   }
   const receiver = await User.findById(receiverId);
   if (!receiver) {
     return next(new ApiError(BAD_REQUEST, "User not found"));
   }
-  if(!receiver.verified){
+  if (!receiver.verified) {
     return next(new ApiError(BAD_REQUEST, "User not verified"));
   }
   let userOneId = new mongoose.Types.ObjectId(req.user._id);
   let userTwoId = new mongoose.Types.ObjectId(receiverId);
-  if(userOneId > userTwoId){
+  if (userOneId > userTwoId) {
     [userOneId, userTwoId] = [userTwoId, userOneId];
   }
-  const isFriendshipExists = await Friendship.findOne({userOneId, userTwoId});
-  if(isFriendshipExists){
-    return next(new ApiError(BAD_REQUEST, "Friendship already exists, please refresh the page"));
+  const isFriendshipExists = await Friendship.findOne({ userOneId, userTwoId });
+  if (isFriendshipExists) {
+    return next(
+      new ApiError(
+        BAD_REQUEST,
+        "Friendship already exists, please refresh the page"
+      )
+    );
   }
   const friendship = await Friendship.create({
     userOneId: req.user._id,
     userTwoId: receiver._id,
     requestSenderId: req.user._id,
-    receiverId: receiver._id
+    receiverId: receiver._id,
   });
   if (!friendship) {
     return next(new ApiError(INTERNAL_SERVER_ERROR, "Something went wrong"));
@@ -62,18 +69,22 @@ export const createFriendRequest = asyncHandler(async (req, res, next) => {
   const isNotificationEnabled = receiver.settings.notificationEnabled;
   await produceNotification(KAFKA_NOTIFICATION_TOPIC, {
     tokens: receiver.pushTokens,
-    options: isNotificationEnabled ? {
-      title: "Friend Request",
-      body: `${req.user.name} sent you a friend request`,
-    }:undefined,
+    options: isNotificationEnabled
+      ? {
+          title: "Friend Request",
+          body: `${req.user.name} sent you a friend request`,
+        }
+      : undefined,
     data: {
       type: "friend_request",
       friendshipId: friendship._id,
     },
   });
-  res
-    .status(OK)
-    .json(new ApiResponse(OK, "Friend request created", {friendshipId: friendship._id}));
+  res.status(OK).json(
+    new ApiResponse(OK, "Friend request created", {
+      friendshipId: friendship._id,
+    })
+  );
 });
 
 export const respondFriendRequest = asyncHandler(async (req, res, next) => {
@@ -84,24 +95,26 @@ export const respondFriendRequest = asyncHandler(async (req, res, next) => {
   if (!["accepted", "rejected"].includes(status)) {
     return next(new ApiError(BAD_REQUEST, "Invalid status"));
   }
-  let friendship = await Friendship.findById(friendshipId).populate("requestSenderId");
+  let friendship = await Friendship.findById(friendshipId).populate(
+    "requestSenderId"
+  );
   if (!friendship) {
     return next(new ApiError(NOT_FOUND, "Friendship not found"));
   }
-  if(friendship.status === "accepted"){
-    return res
-      .status(OK)
-      .json(new ApiResponse(OK, "Friend request already accepted", {alreadyFriends:true}));
+  if (friendship.status === "accepted") {
+    return res.status(OK).json(
+      new ApiResponse(OK, "Friend request already accepted", {
+        alreadyFriends: true,
+      })
+    );
   }
-  if(status === "rejected"){
+  if (status === "rejected") {
     await Friendship.findByIdAndDelete(friendshipId);
-    return res
-      .status(OK)
-      .json(new ApiResponse(OK, "Friend request rejected"));
+    return res.status(OK).json(new ApiResponse(OK, "Friend request rejected"));
   }
   const reqSender = friendship.requestSenderId;
   friendship.status = status;
-  await friendship.save(); 
+  await friendship.save();
 
   const notification = await Notification.create({
     userId: reqSender._id,
@@ -118,19 +131,19 @@ export const respondFriendRequest = asyncHandler(async (req, res, next) => {
   const isNotificationEnabled = reqSender.settings.notificationEnabled;
   await produceNotification(KAFKA_NOTIFICATION_TOPIC, {
     tokens: reqSender.pushTokens,
-    options: isNotificationEnabled ? {
-      title: "Friend Request",
-      body: `${req.user.name} accepted your friend request`,
-    }:undefined,
+    options: isNotificationEnabled
+      ? {
+          title: "Friend Request",
+          body: `${req.user.name} accepted your friend request`,
+        }
+      : undefined,
     data: {
       type: "friend_request_accepted",
       friendshipId: friendship._id,
     },
   });
-  
-  res
-    .status(OK)
-    .json(new ApiResponse(OK, "Friend request responded"));
+
+  res.status(OK).json(new ApiResponse(OK, "Friend request responded"));
 });
 
 export const cancelFriendRequest = asyncHandler(async (req, res, next) => {
@@ -142,24 +155,26 @@ export const cancelFriendRequest = asyncHandler(async (req, res, next) => {
   if (!friendship) {
     return next(new ApiError(NOT_FOUND, "Friendship not found"));
   }
-  if(friendship.status === "accepted"){
-    return res
-      .status(OK)
-      .json(new ApiResponse(OK, "Friend request already accepted", {alreadyFriends:true}));
+  if (friendship.status === "accepted") {
+    return res.status(OK).json(
+      new ApiResponse(OK, "Friend request already accepted", {
+        alreadyFriends: true,
+      })
+    );
   }
-  if(friendship.requestSenderId.toString() !== req.user._id.toString()){
-    return next(new ApiError(BAD_REQUEST, "You can only cancel your own friend request"));
+  if (friendship.requestSenderId.toString() !== req.user._id.toString()) {
+    return next(
+      new ApiError(BAD_REQUEST, "You can only cancel your own friend request")
+    );
   }
   await Friendship.findByIdAndDelete(friendshipId);
-  res
-    .status(OK)
-    .json(new ApiResponse(OK, "Friend request canceled"));
+  res.status(OK).json(new ApiResponse(OK, "Friend request canceled"));
 });
 
 export const getFriendRequests = asyncHandler(async (req, res, next) => {
   const limit = 50;
-  let page = Number(req.query.page);
-  if(isNaN(page) || page < 1) page = 1;
+  let page = Number(req.query?.page);
+  if (isNaN(page) || page < 1) page = 1;
   const userId = req.user?._id?.toString();
   const friendRequestsResults = await Friendship.aggregate([
     {
@@ -175,8 +190,11 @@ export const getFriendRequests = asyncHandler(async (req, res, next) => {
       },
     },
     {
-      $facet:{
-        data:[
+      $facet: {
+        data: [
+          { $sort: { createdAt: -1 } },
+          { $skip: (page - 1) * limit },
+          { $limit: limit },
           {
             $lookup: {
               from: "users",
@@ -200,37 +218,129 @@ export const getFriendRequests = asyncHandler(async (req, res, next) => {
               sender: 1,
               createdAt: 1,
               updatedAt: 1,
-            }
+            },
           },
-          {$sort: {createdAt: -1}},
-          {$skip: (page - 1) * limit},
-          {$limit: limit},
         ],
         total: [{ $count: "count" }],
-      }
-    }
+      },
+    },
   ]);
-  const friendRequests = friendRequestsResults[0].data;  
-  const totalRequests = friendRequestsResults[0].total[0] ? friendRequestsResults[0].total[0].count : 0;
-  const hasMore = (page * limit) < totalRequests;
-  res
-    .status(OK)
-    .json(new ApiResponse(OK, "Get friend request success", {friendRequests, totalRequests, hasMore}));
+  const friendRequests = friendRequestsResults[0].data;
+  const totalRequests = friendRequestsResults[0].total[0]
+    ? friendRequestsResults[0].total[0].count
+    : 0;
+  const hasMore = page * limit < totalRequests;
+  res.status(OK).json(
+    new ApiResponse(OK, "Get friend request success", {
+      friendRequests,
+      totalRequests,
+      hasMore,
+    })
+  );
 });
 
 export const getAllFriends = asyncHandler(async (req, res, next) => {
+  const limit = 5;
+  let page = Number(req.query?.page);
+  if (isNaN(page) || page < 1) page = 1;
   const userId = req.user?._id?.toString();
-  const friends = await Friendship.aggregate([
+  const responseData = await Friendship.aggregate([
     {
-      $match:{
-        $and:[
-          {$or:[
-            {userOneId: new mongoose.Types.ObjectId(userId)},
-            {userTwoId: new mongoose.Types.ObjectId(userId)}
-          ]},
-          {status: "accepted"}
-        ]
-      }
+      $match: {
+        $and: [
+          {
+            $or: [
+              { userOneId: new mongoose.Types.ObjectId(userId) },
+              { userTwoId: new mongoose.Types.ObjectId(userId) },
+            ],
+          },
+          { status: "accepted" },
+        ],
+      },
+    },
+    {
+      $facet: {
+        data: [
+          { $skip: (page - 1) * limit },
+          { $limit: limit },
+          {
+            $addFields: {
+              friendId: {
+                $cond: {
+                  if: {
+                    $eq: ["$userOneId", new mongoose.Types.ObjectId(userId)],
+                  },
+                  then: "$userTwoId",
+                  else: "$userOneId",
+                },
+              },
+            },
+          },
+          {
+            $lookup: {
+              from: "users",
+              localField: "friendId",
+              foreignField: "_id",
+              as: "friend",
+              pipeline: [
+                {
+                  $project: {
+                    name: 1,
+                    profilePic: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $unwind: "$friend",
+          },
+          {
+            $project: {
+              status: 1,
+              friend: 1,
+              updatedAt: 1,
+            },
+          },
+        ],
+        total: [{ $count: "count" }],
+      },
+    },
+  ]);
+  const friends = responseData[0].data;
+  const total = responseData[0].total[0] ? responseData[0].total[0].count : 0;
+  const hasMore = page * limit < total;
+  res.status(OK).json(
+    new ApiResponse(OK, "Get all friends success", {
+      friends,
+      total,
+      hasMore,
+    })
+  );
+});
+
+export const searchInFriends = asyncHandler(async (req, res, next) => {
+  const limit = 5;
+  let page = Number(req.query?.page);
+  const query = req.query?.search?.trim() || "";
+  if (!query) {
+    return next(new ApiError(BAD_REQUEST, "Please provide search query"));
+  }
+  if (isNaN(page) || page < 1) page = 1;
+  const userId = req.user?._id?.toString();
+  const responseData = await Friendship.aggregate([
+    {
+      $match: {
+        $and: [
+          {
+            $or: [
+              { userOneId: new mongoose.Types.ObjectId(userId) },
+              { userTwoId: new mongoose.Types.ObjectId(userId) },
+            ],
+          },
+          { status: "accepted" },
+        ],
+      },
     },
     {
       $addFields: {
@@ -261,38 +371,61 @@ export const getAllFriends = asyncHandler(async (req, res, next) => {
         ],
       },
     },
+    {$unwind: "$friend"},
+    {$match:{"friend.name": {$regex: query, $options: "i"}}},
     {
-      $unwind: "$friend",
+      $facet: {
+        data: [
+          { $skip: (page - 1) * limit },
+          { $limit: limit },
+          {
+            $project: {
+              status: 1,
+              friend: 1,
+              updatedAt: 1,
+            },
+          },
+        ],
+        total: [{ $count: "count" }],
+      },
     },
   ]);
-  res
-    .status(OK)
-    .json(new ApiResponse(OK, "Get all friends success", friends));
+  const friends = responseData[0].data;
+  const total = responseData[0].total[0] ? responseData[0].total[0].count : 0;
+  const hasMore = page * limit < total;
+  res.status(OK).json(
+    new ApiResponse(OK, "Search in friends success", {
+      friends,
+      total,
+      hasMore,
+    })
+  );
 });
 
 export const getAllFriendReqSent = asyncHandler(async (req, res, next) => {
   const userId = req.user?._id?.toString();
   const sentRequests = await Friendship.aggregate([
     {
-      $match:{
-        $and:[
-          {status: "pending"},
-          {requestSenderId: new mongoose.Types.ObjectId(userId)}
-        ]
-      }
+      $match: {
+        $and: [
+          { status: "pending" },
+          { requestSenderId: new mongoose.Types.ObjectId(userId) },
+        ],
+      },
     },
+    { $sort: { createdAt: -1 } },
     {
-      $addFields:{
-        receiverId:{
-          $cond:{
+      $addFields: {
+        receiverId: {
+          $cond: {
             if: {
               $eq: ["$userOneId", new mongoose.Types.ObjectId(userId)],
             },
             then: "$userTwoId",
             else: "$userOneId",
-          }
-        }
-      }
+          },
+        },
+      },
     },
     {
       $lookup: {
@@ -312,6 +445,13 @@ export const getAllFriendReqSent = asyncHandler(async (req, res, next) => {
     },
     {
       $unwind: "$receiver",
+    },
+    {
+      $project: {
+        status: 1,
+        receiver: 1,
+        createdAt: 1,
+      },
     },
   ]);
   res
--- src/routes/friendship.route.js
+++ src/routes/friendship.route.js
@@ -6,7 +6,8 @@ import {
   getAllFriendReqSent, 
   getAllFriends, 
   getFriendRequests, 
-  respondFriendRequest 
+  respondFriendRequest, 
+  searchInFriends
 } from "../controllers/friendship.controller.js";
 
 const router = express.Router();  
@@ -16,5 +17,6 @@ router.route("/request/all").get(authenticate, getFriendRequests);
 router.route("/request/respond").post(authenticate, respondFriendRequest);
 router.route("/request/cancel/:friendshipId").delete(authenticate, cancelFriendRequest);
 router.route("/friends").get(authenticate, getAllFriends);
+router.route("/friends/search").get(authenticate, searchInFriends);
 router.route("/request/sent").get(authenticate, getAllFriendReqSent);
 export default router;
